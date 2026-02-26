9d39c2164720e832d9b12acf2a8c7ba3bb87f60a
Sakib Ahmed <sakibahmedsadhin@gmail.com>
2026-02-27 03:37:44 +0600

    second

--- server/storage.ts
+++
@@ -1,9 +0,0 @@
-export interface IStorage {
-  // No storage operations required
-}
-
-export class MemStorage implements IStorage {
-  constructor() {}
-}
-
-export const storage = new MemStorage();
